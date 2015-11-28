30ee275f5879d3814214dc1d3844ca12f3524877
Blarry Wang <blarrywang@gmail.com>
2015-11-28 10:34:45 +0000

    New feature added

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
-
+<link rel=stylesheet type=text/css href="assets/css/theme.css">
 </head>
 <body>
-  <p>heck</p>
+  <div id="gameContainer">
+    <p>It's good</p>
+  </div>
 </body>
 </html>
